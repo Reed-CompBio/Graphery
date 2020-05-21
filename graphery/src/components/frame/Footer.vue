3ebb40ca96950534d9c4a2b9a9ae8635696a2472
shl-cx <i@wh0.is>
2020-05-21 16:33:57 +0800

    footer style and logo font change
    css is need to be merged into one single file.

--- graphery/src/components/frame/Footer.vue
+++ graphery/src/components/frame/Footer.vue
@@ -1,16 +1,16 @@
 <template>
   <v-footer absolute>
-    <v-card-text v-html="footerHtml" class="text-center"></v-card-text>
+    <v-card-text v-html="footerHtml" class="text-center" id="footer"></v-card-text>
   </v-footer>
 </template>
 
 <script>
-  import { mapState } from 'vuex';
-  export default {
-    computed: {
-      ...mapState({
-        footerHtml: (state) => state.meta.footerHTML,
-      }),
-    },
-  };
+import { mapState } from 'vuex';
+export default {
+  computed: {
+    ...mapState({
+      footerHtml: (state) => state.meta.footerHTML,
+    }),
+  },
+};
 </script>
