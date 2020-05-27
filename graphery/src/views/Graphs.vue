9683668e35f6776c738949ff4c0f991657247afa
Larry Zeng <princeflickersoul@gmail.com>
2020-05-27 09:45:51 -0700

    layout framework

--- graphery/src/views/Graphs.vue
+++ graphery/src/views/Graphs.vue
@@ -1,5 +1,24 @@
 <template>
-  <div>
-    This is a page for graphs
-  </div>
+  <MaterialPage>
+    <div id="search-section">Search Bar is here</div>
+    <div id="content-section">
+      <div class="row">
+        <div id="filter-section" class="col-3">
+          Filter section
+        </div>
+        <div class="col-1"><q-separator vertical /></div>
+        <div id="tutorial-list" class="col-8">
+          Graph list section
+        </div>
+      </div>
+    </div>
+  </MaterialPage>
 </template>
+
+<script>
+  export default {
+    components: {
+      MaterialPage: () => import('@/components/framework/MaterialPage.vue'),
+    },
+  };
+</script>
--- graphery/src/views/Tutorials.vue
+++ graphery/src/views/Tutorials.vue
@@ -1,5 +1,24 @@
 <template>
-  <div>
-    This is the page for tutorials
-  </div>
+  <MaterialPage>
+    <div id="search-section">Search Bar is here</div>
+    <div id="content-section">
+      <div class="row">
+        <div id="filter-section" class="col-3">
+          Filter section
+        </div>
+        <div class="col-1"><q-separator vertical /></div>
+        <div id="tutorial-list" class="col-8">
+          Tutorial list section
+        </div>
+      </div>
+    </div>
+  </MaterialPage>
 </template>
+
+<script>
+  export default {
+    components: {
+      MaterialPage: () => import('@/components/framework/MaterialPage.vue'),
+    },
+  };
+</script>
