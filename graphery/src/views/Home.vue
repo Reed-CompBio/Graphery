25580cd7b15661040fde5e5d0bc3487553b38dee
Larry Zeng <princeflickersoul@gmail.com>
2020-05-27 09:36:54 -0700

    added home page

--- graphery/src/views/Home.vue
+++ graphery/src/views/Home.vue
@@ -1,7 +1,24 @@
 <template>
-  <div class="home">
-    This is the home page
-  </div>
+  <MaterialPage>
+    <div class="flex-center text-center">
+      <h3>Welcome to Graphery</h3>
+      <p class="text-subtitle1">This is a graph algorithm tutorial website.</p>
+    </div>
+    <q-separator></q-separator>
+    <div class="row q-my-md">
+      <q-btn size="lg" class="q-ml-auto">Tutorials</q-btn>
+      <q-btn size="lg" class="q-mx-lg">Graphs</q-btn>
+      <q-btn size="lg" class="q-mr-auto">Account</q-btn>
+    </div>
+    <div class="flex-center text-center text-body1">
+      <p>
+        You can check out our tutorials and play with graphs.
+      </p>
+      <p>
+        You don't have an account yet, the settings will only be stored locally.
+      </p>
+    </div>
+  </MaterialPage>
 </template>
 
 <script>
@@ -9,5 +26,8 @@
 
   export default {
     name: 'Home',
+    components: {
+      MaterialPage: () => import('@/components/framework/MaterialPage.vue'),
+    },
   };
 </script>
