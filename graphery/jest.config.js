f13294979e3d8494a3908c4ca6bcb5a47903d422
Larry Zeng <princeflickersoul@gmail.com>
2020-05-27 23:08:45 -0700

    add code coverage

--- graphery/jest.config.js
+++ graphery/jest.config.js
@@ -1,3 +1,5 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+  collectCoverage: true,
+  collectCoverageFrom: ['**/*.{js,jsx,ts,vue}', '!**/node_modules/**'],
 };
