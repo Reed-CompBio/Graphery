1edf6e2e33d8371ff460d71f4be0ab51392bb081
Larry Zeng <princeflickersoul@gmail.com>
2020-05-28 17:23:25 -0700

    minor changes

--- graphery/src/store/states/state.ts
+++ graphery/src/store/states/state.ts
@@ -78,6 +78,7 @@ export interface SettingInfos {
   motionBlurEnabled: boolean;
   motionSensitivityLevel: number;
   graphSplitPos: number;
+  // TODO maybe add a motion sensitivity?
 
   // editor settings
   tabNum: number;
--- graphery/src/views/Account.vue
+++ graphery/src/views/Account.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>
-    This is the loggin page
-  </div>
+  <MaterialPage>
+    <div>
+      <h3 class="shorter-h">
+        Account
+      </h3>
+    </div>
+    <div>
+      <p>Not implemented</p>
+    </div>
+  </MaterialPage>
 </template>
+
+<script>
+  export default {
+    components: {
+      MaterialPage: () => import('@/components/framework/MaterialPage.vue'),
+    },
+  };
+</script>
