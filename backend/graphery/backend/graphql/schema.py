d5a2593f787309244f9ac2d8209a677a267c1ce4
Poppy <13360148+poppy-poppy@users.noreply.github.com>
2020-06-17 11:50:32 -0700

    minor changes

--- backend/graphery/backend/graphql/schema.py
+++ backend/graphery/backend/graphql/schema.py
@@ -2,11 +2,11 @@ import graphene
 import graphql_jwt
 
 
-class Query(object):
+class Query(graphene.ObjectType):
     pass
 
 
-class Mutation(object):
+class Mutation(graphene.ObjectType):
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
