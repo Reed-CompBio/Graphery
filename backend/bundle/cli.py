9c2663fc575e011ae97b01c921ac146dc170f9cd
Poppy <13360148+poppy-poppy@users.noreply.github.com>
2020-06-20 03:10:28 -0700

    try cli

--- backend/bundle/cli.py
+++ backend/bundle/cli.py
@@ -1,3 +1,24 @@
+from django.core import management
+from django.core.management.commands import loaddata
+from django.conf import settings
+from graphery.graphery.settings import test
+from django import setup as django_setup
+
+import pathlib
+import sys
+import os
+
+
+def load_data_test():
+    django_root = pathlib.Path('../graphery/')
+    print(django_root.exists())
+    sys.path.append(str(django_root))
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.test")
+    # settings.configure(default_settings=test)
+    django_setup()
+    # management.call_command(loaddata.Command(), 'test',
+    #                         app_label='backend')
+
+
 if __name__ == '__main__':
-    import os
-    print(os.path.dirname(__file__))
+    load_data_test()
