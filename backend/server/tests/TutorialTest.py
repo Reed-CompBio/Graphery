3342ae9779a430612942068ceaef545a21dfd120
Poppy <13360148+poppy-poppy@users.noreply.github.com>
2020-06-21 23:43:27 -0700

    some tests

--- backend/server/tests/TutorialTest.py
+++ backend/server/tests/TutorialTest.py
@@ -1,8 +1,11 @@
 from django.db import IntegrityError
 from django.test import TestCase
-from backend.models import Category, Tutorial
+from backend.models import *
 from graphene_django.utils.testing import GraphQLTestCase
 from graphery.schema import schema
+from graphene.test import Client
+from graphql_jwt.testcases import JSONWebTokenTestCase
+import json
 
 
 class CategoryTest(TestCase):
@@ -37,9 +40,68 @@ class TutorialAnchorTest(TestCase):
         self.assertEqual(tutorial.categories.all()[0].category, category.category)
 
 
-class GraphQLAPITest(GraphQLTestCase):
+class GraphQLAPITest(GraphQLTestCase, JSONWebTokenTestCase):
     GRAPHQL_SCHEMA = schema
     fixtures = ['test_data.json', ]
 
-    # def
+    def test_fixture(self):
+        user = User.objects.get(username='default_user')
+        self.assertEqual(user.email, 'default@reed.edu')
+
+    def test_tutorial_anchor_get(self):
+        client = Client(schema=schema)
+        query = '''
+            query getTutorialPost($url: String, $translation: String="en-us") {
+                tutorial(url: $url) {   
+                content(translation: $translation) {
+                  authors
+                  contentMd
+                  contentHtml
+                }
+                code {
+                  id
+                  code
+                }
+                graphSet {
+                  id
+                  cyjs
+                  execresultjsonSet {
+                    id
+                  }
+                }
+              }
+            }
+        '''
+        variables = {
+            "url": "test-tutorial3",
+            "translation": "zhcn"
+        }
+        response = client.execute(query, variables=variables)
+        print(response)
+
+    def test_login_and_get_user_info(self):
+        response = self.client.execute('''
+            mutation {
+                tokenAuth(username: "default_user", password: "password") {
+                    payload
+                    refreshExpiresIn
+                    token
+                }
+            }
+        ''')
+        print(response)
+        info_response = self.client.execute('''
+            query {
+                userInfo {
+                    id
+                    email
+                    role
+                    isVerified
+                    dateJoined
+                }
+            }
+        ''')
+        print(info_response)
+
+    # def test_get_user_info(self):
 
