3858ebe27028743c6813b0d9352632f204f4dcbd
Poppy <13360148+poppy-poppy@users.noreply.github.com>
2020-07-16 14:14:47 -0700

    finished migration

--- backend/server/cli_utils/cli_controller.py
+++ backend/server/cli_utils/cli_controller.py
@@ -1,31 +1,38 @@
-from typing import List, Callable
+from typing import List, Tuple, Type
 
 from prompt_toolkit import print_formatted_text
 
 from cli_utils.cli_ui import run_interruptable_checkbox_dialog, new_session
+from cli_utils.command_helpers.code_and_exec_result_creator import CodeExecCreator
+from cli_utils.command_helpers.command_base import CommandBase
+from cli_utils.command_helpers.graph_content_creator import GraphContentCreator
+from cli_utils.command_helpers.graph_creator import GraphCreator
+from cli_utils.command_helpers.tutorial_anchor_creator import TutorialAnchorCreator
+from cli_utils.command_helpers.tutorial_content_creator import TutorialContentCreator
+from cli_utils.command_helpers.user_creator import UserCreator
 
 
 class CommandWrapper:
-    action_values = [
-        (create_user, 'Create User'),
-        (create_tutorial_anchor, 'Create Tutorial Anchor'),
-        (create_locale_md, 'Create Tutorial Content From Markdown Files'),
-        (create_graph, 'Create Graph From Jsons'),
-        (create_graph_content_trans, 'Create Graph Info Content From Markdown Files'),
-        (create_code_obj, 'Create Code and Execution Result Records')
+    action_values: List[Tuple[Type[CommandBase], str]] = [
+        (UserCreator, 'Create User'),
+        (TutorialAnchorCreator, 'Create Tutorial Anchor'),
+        (TutorialContentCreator, 'Create Tutorial Content From Markdown Files'),
+        (GraphCreator, 'Create Graph From Jsons'),
+        (GraphContentCreator, 'Create Graph Info Content From Markdown Files'),
+        (CodeExecCreator, 'Create Code and Execution Result Records')
     ]
 
     # TODO use instance instead of class, so that I can have a recent create list.
     @classmethod
     @new_session('create')
     def create(cls):
-        action_choices: List[Callable] = run_interruptable_checkbox_dialog(
+        action_choices: List[Type] = run_interruptable_checkbox_dialog(
             text='Please choose what you want to do',
             values=cls.action_values
         )
 
-        for action in action_choices:
-            action()
+        for action_class in action_choices:
+            action_class()()
 
     @staticmethod
     def add():
