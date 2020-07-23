ec1c50a97295b0f78c245e9ce1078f0ec43c454c
Poppy <13360148+poppy-poppy@users.noreply.github.com>
2020-07-22 18:23:45 -0700

    check point of tutorial content list

---
+++ graphery/src/components/ControlPanel/TutorialContentList.vue
@@ -0,0 +1,23 @@
+<template>
+  <ControlPanelContentFrame>
+    <template v-slot:title>
+      Tutorial Contents
+    </template>
+    <template>
+      <q-table></q-table>
+    </template>
+  </ControlPanelContentFrame>
+</template>
+
+<script>
+  export default {
+    components: {
+      ControlPanelContentFrame: () => import('./ControlPanelContentFrame'),
+    },
+    data() {
+      return {
+        columns: [],
+      };
+    },
+  };
+</script>
