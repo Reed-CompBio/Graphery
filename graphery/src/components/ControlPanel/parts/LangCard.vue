0b0c02acf00c8b3cf2b6a6a38e39ce1897b0143f
Poppy <13360148+poppy-poppy@users.noreply.github.com>
2020-07-29 14:23:47 -0700

    added lang card

---
+++ graphery/src/components/ControlPanel/parts/LangCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <InfoCard>
+    <template v-slot:title>
+      Language
+    </template>
+    <div class="text-center">
+      {{ lang }}
+    </div>
+  </InfoCard>
+</template>
+
+<script>
+  import InfoCard from '../parts/InfoCard.vue';
+  export default {
+    props: ['lang'],
+    components: {
+      InfoCard,
+    },
+  };
+</script>
