95ffe54de1658d47fbdeab56c7f665355771fcb8
Poppy <13360148+poppy-poppy@users.noreply.github.com>
2020-08-09 14:33:16 -0700

    use upload table in uploads list

--- graphery/src/components/ControlPanel/lists/UploadsList.vue
+++ graphery/src/components/ControlPanel/lists/UploadsList.vue
@@ -4,24 +4,7 @@
       Uploads
     </template>
     <template>
-      <q-table
-        :data="tableContent"
-        :columns="columns"
-        :pagination="pagination"
-        row-key="name"
-        grid
-        hide-header
-        :loading="loadingContent"
-      >
-        <template v-slot:item="props">
-          <div class="q-pa-xs col-xs-12 col-sm-4 col-md-3 col-lg-2">
-            <UploadDisplayCard
-              :resource-link="props.row.relativeUrl"
-              @showUploadInfo="showUploadInfo"
-            />
-          </div>
-        </template>
-      </q-table>
+      <UploadTable @showUploadInfo="showUploadInfo" />
       <UploadInfoWindow
         :resource-link="infoWindowIntel"
         v-model="showUploadInfoWindow"
@@ -31,65 +14,22 @@
 </template>
 
 <script>
-  import loadingMixin from '../mixins/LoadingMixin';
-  import UploadDisplayCard from '@/components/ControlPanel/parts/cards/UploadDisplayCard';
-  import { apiCaller } from '@/services/apis';
-  import { fetchUploads } from '@/services/queries';
-  import { errorDialog } from '@/services/helpers';
-  import UploadInfoWindow from '@/components/ControlPanel/editors/UploadInfoWindow';
-
   export default {
-    mixins: [loadingMixin],
     components: {
-      UploadInfoWindow,
-      UploadDisplayCard,
+      UploadTable: () =>
+        import('@/components/ControlPanel/parts/upload/UploadTable'),
+      UploadInfoWindow: () =>
+        import('@/components/ControlPanel/editors/UploadInfoWindow'),
       ControlPanelContentFrame: () =>
         import('../frames/ControlPanelContentFrame'),
     },
     data() {
       return {
-        columns: [
-          {
-            name: 'relativeUrl',
-            label: 'relativeUrl',
-            field: 'relativeUrl',
-          },
-          {
-            name: 'id',
-            label: 'id',
-            field: 'id',
-            required: true,
-          },
-        ],
-        pagination: {
-          sortBy: '',
-          rowsPerPage: 20,
-        },
-        tableContent: [],
         showUploadInfoWindow: false,
         infoWindowIntel: '',
       };
     },
     methods: {
-      fetchUploadsContent() {
-        this.startLoading();
-        apiCaller(fetchUploads)
-          .then((data) => {
-            if (!data || !('allUploads' in data)) {
-              throw Error('Invalid data returned.');
-            }
-
-            this.tableContent = data.allUploads;
-          })
-          .catch((err) => {
-            errorDialog({
-              message: `An error occurs during fetching uploads. ${err}`,
-            });
-          })
-          .finally(() => {
-            this.finishedLoading();
-          });
-      },
       showInfo() {
         this.showUploadInfoWindow = true;
       },
@@ -100,9 +40,6 @@
         this.changeInfoWindowIntel(url);
         this.showInfo();
       },
-    },
-    mounted() {
-      this.fetchUploadsContent();
     },
   };
 </script>
