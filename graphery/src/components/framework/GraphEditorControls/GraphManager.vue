20a192671199817b2f2b39fb1781f7ef4401db65
Poppy <13360148+poppy-poppy@users.noreply.github.com>
2020-08-19 10:25:25 -0700

    added graph manager

---
+++ graphery/src/components/framework/GraphEditorControls/GraphManager.vue
@@ -0,0 +1,25 @@
+<script>
+  import { mapGetters } from 'vuex';
+
+  export default {
+    computed: {
+      ...mapGetters('graphs', ['getCurrentGraphId', 'getCurrentGraphObject']),
+      currentGraphId: {
+        set(d) {
+          this.$store.commit('graphs/LOAD_CURRENT_GRAPH_ID', d);
+        },
+        get() {
+          return this.getCurrentGraphId;
+        },
+      },
+      currentGraphObject() {
+        return this.getCurrentGraphObject();
+      },
+    },
+    methods: {
+      loadGraphObjectListFromMatched(graphList) {
+        this.$store.commit('graphs/LOAD_GRAPH_OBJECT_LIST', graphList);
+      },
+    },
+  };
+</script>
