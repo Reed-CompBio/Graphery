93603c2ca21d5e794f1f4d20b0e0856b3db04eff
Poppy <13360148+poppy-poppy@users.noreply.github.com>
2020-08-19 09:19:56 -0700

    added json manager

---
+++ graphery/src/components/framework/GraphEditorControls/ResultJsonManager.vue
@@ -0,0 +1,32 @@
+<script>
+  import { mapGetters } from 'vuex';
+  export default {
+    data() {
+      return {
+        resultJsonPositions: {},
+      };
+    },
+    computed: {
+      ...mapGetters('rj', ['getCurrentJsonObject', 'getCurrentJsonString']),
+    },
+    methods: {
+      getIdFromGraphIdAndCodeId(graphId, codeId) {
+        return `${graphId}-${codeId}`;
+      },
+      updateResultJsonPosition(graphId, codeId, pos) {
+        this.resultJsonPositions[
+          this.getIdFromGraphIdAndCodeId(graphId, codeId)
+        ] = pos;
+      },
+      initResultJsonPositions(codeIds, graphIds) {
+        for (const graphId of graphIds) {
+          for (const codeId of codeIds) {
+            this.resultJsonPositions[
+              this.getIdFromGraphIdAndCodeId(graphId, codeId)
+            ] = 1;
+          }
+        }
+      },
+    },
+  };
+</script>
