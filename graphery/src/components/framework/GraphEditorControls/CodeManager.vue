4adae019635b8c2aa3e87af2c24b6ba4bdff7d7d
Poppy <13360148+poppy-poppy@users.noreply.github.com>
2020-08-19 10:25:13 -0700

    added code manager

---
+++ graphery/src/components/framework/GraphEditorControls/CodeManager.vue
@@ -0,0 +1,28 @@
+<script>
+  import { mapGetters } from 'vuex';
+
+  export default {
+    computed: {
+      ...mapGetters('code', ['getCurrentCodeId', 'getCurrentCodeObject']),
+      currentCodeId: {
+        set(d) {
+          this.$store.commit('code/LOAD_CURRENT_CODE_ID', d);
+        },
+        get() {
+          return this.getCurrentCodeId;
+        },
+      },
+      currentCodeObject() {
+        return this.getCurrentCodeObject;
+      },
+    },
+    methods: {
+      loadCodeObjectListFromMatched(codeList) {
+        this.$store.commit('code/LOAD_CODE_LIST', codeList);
+      },
+      updateCode(newCode) {
+        this.$store.commit('code/CHANGE_CODE_CONTENT', newCode);
+      },
+    },
+  };
+</script>
