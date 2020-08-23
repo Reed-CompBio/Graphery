27546436226271ed781992b3989b15c5b357fdb7
Poppy <13360148+poppy-poppy@users.noreply.github.com>
2020-08-23 10:39:53 -0700

    update launch script

--- backend/launch.bat
+++ backend/launch.bat
@@ -1,2 +1,15 @@
+@echo off
 py -3 --version
+if errorlevel 1 goto errorNoPython
+
+:execLocalServer
 py -3 "%~dp0user_server.py"
+pause
+exit
+
+:errorNoPython
+echo.
+echo Error^: Python not installed
+pause
+exit
+
