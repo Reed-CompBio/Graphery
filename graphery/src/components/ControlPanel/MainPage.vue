e382f266e6d4dee9cf5aa3322e5bee2518902f85
Poppy <13360148+poppy-poppy@users.noreply.github.com>
2020-08-28 17:52:58 -0700

    added breakpoint test

--- graphery/src/components/ControlPanel/MainPage.vue
+++ graphery/src/components/ControlPanel/MainPage.vue
@@ -20,6 +20,10 @@
     data() {
       return {
         exampleMd:
+          '@12@\n' +
+          '@13@@14@\n' +
+          '@12.56@\n' +
+          '@sdafew@\n' +
           '# h1 Heading 8-)\n' +
           '## h2 Heading\n' +
           '### h3 Heading\n' +
