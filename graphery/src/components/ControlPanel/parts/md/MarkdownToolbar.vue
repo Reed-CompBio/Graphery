abe4452464b2a5766d6eaafa4f6f2b6ee1c2b1a6
Poppy <13360148+poppy-poppy@users.noreply.github.com>
2020-08-28 17:41:45 -0700

    custom markdown editor

---
+++ graphery/src/components/ControlPanel/parts/md/MarkdownToolbar.vue
@@ -0,0 +1,6 @@
+<template>
+  <q-bar class="q-py-sm q-px-md">
+    <q-space />
+    <q-btn dense flat icon="publish" />
+  </q-bar>
+</template>
---
+++ graphery/src/components/ControlPanel/parts/md/MonacoMarkdown.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="full-width">
+    <div class="row full-width">
+      <MarkdownToolbar class="full-width" />
+    </div>
+    <div class="row full-width" style="height: 50vh;">
+      <q-splitter v-model="splitterPos" class="full-width">
+        <template v-slot:before>
+          <Editor
+            ref="editor"
+            class="full-height"
+            lang="markdown"
+            :wrapLine="true"
+            :miniMapEnable="true"
+            @editorContentChanged="onEditorContentChanged"
+            @editorScrollChanged="onEditorScrollChanged"
+          />
+        </template>
+        <template v-slot:separator>
+          <SplitterSeparator />
+        </template>
+        <template v-slot:after>
+          <div class="full-height full-width">
+            <q-scroll-area
+              class=" q-px-lg full-height full-width"
+              ref="scrollArea"
+            >
+              <MarkdownSection
+                class="q-my-lg"
+                ref="markdownSection"
+                :markdown-raw="rawMarkdown"
+                :highlight="true"
+                :render-breakpoint="true"
+                @processedHtmlChanged="onProcessedHtmlChanged"
+                @breakpointClicked="onBreakpointClicked"
+              />
+            </q-scroll-area>
+          </div>
+        </template>
+      </q-splitter>
+    </div>
+  </div>
+</template>
+<script>
+  import Editor from '@/components/framework/Editor';
+  import MarkdownSection from '@/components/framework/MarkdownSection';
+  import { successDialog } from '@/services/helpers';
+  import { QScrollArea } from 'quasar';
+
+  export default {
+    props: {
+      initValue: {
+        type: String,
+        default: '',
+      },
+    },
+    components: {
+      MarkdownSection,
+      SplitterSeparator: () =>
+        import('@/components/framework/SplitterSeparator'),
+      MarkdownToolbar: () =>
+        import('@/components/ControlPanel/parts/md/MarkdownToolbar'),
+      Editor,
+      QScrollArea,
+    },
+    data() {
+      return {
+        rawMarkdown: this.initValue,
+        splitterPos: 60,
+      };
+    },
+    methods: {
+      onEditorContentChanged(content) {
+        this.rawMarkdown = content;
+      },
+      onBreakpointClicked(position) {
+        successDialog({
+          message: `breakpoint clicked ${position}`,
+        });
+      },
+      onProcessedHtmlChanged(processedHtml) {
+        this.$emit('change', this.rawMarkdown, processedHtml);
+      },
+      onEditorScrollChanged(percentage) {
+        // console.log('scroll to ', percentage);
+      },
+    },
+  };
+</script>
