a32363518647b64fa44b492d8adb2c0a26a7d31a
Poppy <13360148+poppy-poppy@users.noreply.github.com>
2020-08-28 20:59:44 -0700

    added markdown syntax info

--- graphery/src/components/ControlPanel/parts/EditorHowTo.vue
+++ graphery/src/components/ControlPanel/parts/EditorHowTo.vue
@@ -1,7 +1,7 @@
 <template>
   <q-card>
     <q-card-section>
-      How To
+      Supported Markdown Syntax
       <q-btn
         color="grey"
         round
@@ -15,7 +15,7 @@
       <div v-show="expanded">
         <q-separator />
         <q-card-section>
-          <MarkdownSection :input-html="html" />
+          <MarkdownSection :markdown-raw="helpMarkdown" />
         </q-card-section>
       </div>
     </q-slide-transition>
@@ -27,8 +27,211 @@
     components: { MarkdownSection },
     data() {
       return {
-        html:
-          '<table> <thead> <tr> <th>key</th> <th style="text-align:center">keycode</th> <th style="text-align:center">功能</th> </tr> </thead> <tbody> <tr> <td>F8</td> <td style="text-align:center">119</td> <td style="text-align:center">开启/关闭导航</td> </tr> <tr> <td>F9</td> <td style="text-align:center">120</td> <td style="text-align:center">预览/编辑切换</td> </tr> <tr> <td>F10</td> <td style="text-align:center">121</td> <td style="text-align:center">开启/关闭全屏</td> </tr> <tr> <td>F11</td> <td style="text-align:center">122</td> <td style="text-align:center">开启/关闭阅读模式</td> </tr> <tr> <td>F12</td> <td style="text-align:center">123</td> <td style="text-align:center">单栏/双栏切换</td> </tr> <tr> <td>TAB</td> <td style="text-align:center">9</td> <td style="text-align:center">缩进</td> </tr> <tr> <td>CTRL + S</td> <td style="text-align:center">17 + 83</td> <td style="text-align:center">触发保存</td> </tr> <tr> <td>CTRL + D</td> <td style="text-align:center">17 + 68</td> <td style="text-align:center">删除选中行</td> </tr> <tr> <td>CTRL + Z</td> <td style="text-align:center">17 + 90</td> <td style="text-align:center">上一步</td> </tr> <tr> <td>CTRL + Y</td> <td style="text-align:center">17 + 89</td> <td style="text-align:center">下一步</td> </tr> <tr> <td>CTRL + BreakSpace</td> <td style="text-align:center">17 + 8</td> <td style="text-align:center">清空编辑</td> </tr> <tr> <td>CTRL + B</td> <td style="text-align:center">17 + 66</td> <td style="text-align:center"><strong>加粗</strong></td> </tr> <tr> <td>CTRL + I</td> <td style="text-align:center">17 + 73</td> <td style="text-align:center"><em>斜体</em></td> </tr> <tr> <td>CTRL + H</td> <td style="text-align:center">17 + 72</td> <td style="text-align:center"># 标题</td> </tr> <tr> <td>CTRL + 1</td> <td style="text-align:center">17 + 97 or 49</td> <td style="text-align:center"># 标题</td> </tr> <tr> <td>CTRL + 2</td> <td style="text-align:center">17 + 98 or 50</td> <td style="text-align:center">## 标题</td> </tr> <tr> <td>CTRL + 3</td> <td style="text-align:center">17 + 99 or 51</td> <td style="text-align:center">### 标题</td> </tr> <tr> <td>CTRL + 4</td> <td style="text-align:center">17 + 100 or 52</td> <td style="text-align:center">#### 标题</td> </tr> <tr> <td>CTRL + 5</td> <td style="text-align:center">17 + 101 or 53</td> <td style="text-align:center">##### 标题</td> </tr> <tr> <td>CTRL + 6</td> <td style="text-align:center">17 + 102 or 54</td> <td style="text-align:center">###### 标题</td> </tr> <tr> <td>CTRL + U</td> <td style="text-align:center">17 + 85</td> <td style="text-align:center"><ins>下划线</ins></td> </tr> <tr> <td>CTRL + M</td> <td style="text-align:center">17 + 77</td> <td style="text-align:center"><mark>标记</mark></td> </tr> <tr> <td>CTRL + Q</td> <td style="text-align:center">17 + 81</td> <td style="text-align:center">&gt; 引用</td> </tr> <tr> <td>CTRL + O</td> <td style="text-align:center">17 + 79</td> <td style="text-align:center">1. 有序列表</td> </tr> <tr> <td>CTRL + L</td> <td style="text-align:center">17 + 76</td> <td style="text-align:center"><a href="" target="_blank">链接</a></td> </tr> <tr> <td>CTRL + ALT + S</td> <td style="text-align:center">17 + 18 + 83</td> <td style="text-align:center"><sup>上角标</sup></td> </tr> <tr> <td>CTRL + ALT + U</td> <td style="text-align:center">17 + 18 + 85</td> <td style="text-align:center">- 无序列表</td> </tr> <tr> <td>CTRL + ALT + C</td> <td style="text-align:center">17 + 18 + 67</td> <td style="text-align:center">``` 代码块</td> </tr> <tr> <td>CTRL + ALT + L</td> <td style="text-align:center">17 + 18 + 76</td> <td style="text-align:center"><img src="" alt="图片链接" /></td> </tr> <tr> <td>CTRL + ALT + T</td> <td style="text-align:center">17 + 18 + 84</td> <td style="text-align:center">表格</td> </tr> <tr> <td>CTRL + SHIFT + S</td> <td style="text-align:center">17 + 16 + 83</td> <td style="text-align:center"><sub>下角标</sub></td> </tr> <tr> <td>CTRL + SHIFT + D</td> <td style="text-align:center">17 + 16 + 68</td> <td style="text-align:center"><s>中划线</s></td> </tr> <tr> <td>CTRL + SHIFT + C</td> <td style="text-align:center">17 + 16 + 67</td> <td style="text-align:center">居中</td> </tr> <tr> <td>CTRL + SHIFT + L</td> <td style="text-align:center">17 + 16 + 76</td> <td style="text-align:center">居左</td> </tr> <tr> <td>CTRL + SHIFT + R</td> <td style="text-align:center">17 + 16 + 82</td> <td style="text-align:center">居右</td> </tr> <tr> <td>SHIFT + TAB</td> <td style="text-align:center">16 + 9</td> <td style="text-align:center">取消缩进</td> </tr> </tbody> </table>',
+        helpMarkdown:
+          'Markdown Guide\n' +
+          '===\n' +
+          '> Detailed: [http://commonmark.org/help/](http://commonmark.org/help/)\n' +
+          '\n' +
+          '## **Bold**\n' +
+          '```\n' +
+          '**bold**\n' +
+          '__bold__\n' +
+          '```\n' +
+          '## *Italic*\n' +
+          '```\n' +
+          '*italic*\n' +
+          '_italic_\n' +
+          '```\n' +
+          '## Header\n' +
+          '```\n' +
+          '# h1 #\n' +
+          'h1\n' +
+          '====\n' +
+          '## h2 ##\n' +
+          'h2\n' +
+          '----\n' +
+          '### h3 ###\n' +
+          '#### h4 ####\n' +
+          '##### h5 #####\n' +
+          '###### h6 ######\n' +
+          '```\n' +
+          '## Dividing line\n' +
+          '```\n' +
+          '***\n' +
+          '---\n' +
+          '```\n' +
+          '****\n' +
+          '## ^Super^script & ~Sub~script\n' +
+          '```\n' +
+          'super x^2^\n' +
+          'sub H~2~0\n' +
+          '```\n' +
+          '## ++Underline++ & ~~Strikethrough~~\n' +
+          '```\n' +
+          '++underline++\n' +
+          '~~strikethrough~~\n' +
+          '```\n' +
+          '## ==Mark==\n' +
+          '```\n' +
+          '==mark==\n' +
+          '```\n' +
+          '## Quote\n' +
+          '\n' +
+          '```\n' +
+          '> quote 1\n' +
+          '>> quote 2\n' +
+          '>>> quote 3\n' +
+          '...\n' +
+          '```\n' +
+          '\n' +
+          '## List\n' +
+          '```\n' +
+          'ol\n' +
+          '1.\n' +
+          '2.\n' +
+          '3.\n' +
+          '...\n' +
+          '\n' +
+          'ul\n' +
+          '-\n' +
+          '-\n' +
+          '...\n' +
+          '```\n' +
+          '\n' +
+          '## Todo List\n' +
+          '\n' +
+          '- [x] task 1\n' +
+          '- [ ] task 2\n' +
+          '\n' +
+          '```\n' +
+          '- [x] task 1\n' +
+          '- [ ] task 2\n' +
+          '```\n' +
+          '\n' +
+          '## Link\n' +
+          '```\n' +
+          'Text Link\n' +
+          '[Text](www.baidu.com)\n' +
+          '\n' +
+          'Image Link\n' +
+          '![Text](http://www.image.com)\n' +
+          '```\n' +
+          '## Code\n' +
+          '\\``` type\n' +
+          '\n' +
+          'code block\n' +
+          '\n' +
+          '\\```\n' +
+          '\n' +
+          '\\` code \\`\n' +
+          '\n' +
+          '```python\n' +
+          'def hello():\n' +
+          "    print('hello world')\n" +
+          '\n' +
+          '```\n' +
+          '`code`\n' +
+          '\n' +
+          '## Table\n' +
+          '```\n' +
+          '| th1 | th2 | th3 |\n' +
+          '| :--  | :--: | ----: |\n' +
+          '| left | center | right |\n' +
+          '```\n' +
+          '| th1 | th2 | th3 |\n' +
+          '| :--  | :--: | ----: |\n' +
+          '| left | center | right |\n' +
+          '| ---------------------- | ------------- | ----------------- |\n' +
+          '## Footnote\n' +
+          '```\n' +
+          'hello[^hello]\n' +
+          '```\n' +
+          '\n' +
+          'Look at the bottom[^hello]\n' +
+          '\n' +
+          '[^hello]: footnote\n' +
+          '\n' +
+          '## Emojis\n' +
+          'Detailed: [https://www.webpagefx.com/tools/emoji-cheat-sheet/](https://www.webpagefx.com/tools/emoji-cheat-sheet/)\n' +
+          '```\n' +
+          ':laughing:\n' +
+          ':blush:\n' +
+          ':smiley:\n' +
+          ':)\n' +
+          '...\n' +
+          '```\n' +
+          ':laughing::blush::smiley::)\n' +
+          '\n' +
+          '## $\\KaTeX$ Mathematics\n' +
+          '\n' +
+          'We can render formulas for example：$x_i + y_i = z_i$ and $\\sum_{i=1}^n a_i=0$\n' +
+          'We can also single-line rendering\n' +
+          '$$\\sum_{i=1}^n a_i=0$$\n' +
+          'Detailed: [katex](http://www.intmath.com/cg5/katex-mathjax-comparison.php)和[katex function](https://github.com/Khan/KaTeX/wiki/Function-Support-in-KaTeX)以及[latex](https://math.meta.stackexchange.com/questions/5020/mathjax-basic-tutorial-and-quick-reference)\n' +
+          '\n' +
+          '## Layout\n' +
+          '\n' +
+          '::: hljs-left\n' +
+          '`::: hljs-left`\n' +
+          '`left`\n' +
+          '`:::`\n' +
+          ':::\n' +
+          '\n' +
+          '::: hljs-center\n' +
+          '`::: hljs-center`\n' +
+          '`center`\n' +
+          '`:::`\n' +
+          ':::\n' +
+          '\n' +
+          '::: hljs-right\n' +
+          '`::: hljs-right`\n' +
+          '`right`\n' +
+          '`:::`\n' +
+          ':::\n' +
+          '\n' +
+          '## deflist\n' +
+          '\n' +
+          'Term 1\n' +
+          '\n' +
+          ':   Definition 1\n' +
+          '\n' +
+          'Term 2 with *inline markup*\n' +
+          '\n' +
+          ':   Definition 2\n' +
+          '\n' +
+          '        { some code, part of Definition 2 }\n' +
+          '\n' +
+          '    Third paragraph of definition 2.\n' +
+          '\n' +
+          '```\n' +
+          'Term 1\n' +
+          '\n' +
+          ':   Definition 1\n' +
+          '\n' +
+          'Term 2 with *inline markup*\n' +
+          '\n' +
+          ':   Definition 2\n' +
+          '\n' +
+          '        { some code, part of Definition 2 }\n' +
+          '\n' +
+          '    Third paragraph of definition 2.\n' +
+          '\n' +
+          '```\n' +
+          '\n' +
+          '## abbr\n' +
+          '*[HTML]: Hyper Text Markup Language\n' +
+          '*[W3C]:  World Wide Web Consortium\n' +
+          'The HTML specification\n' +
+          'is maintained by the W3C.\n' +
+          '```\n' +
+          '*[HTML]: Hyper Text Markup Language\n' +
+          '*[W3C]:  World Wide Web Consortium\n' +
+          'The HTML specification\n' +
+          'is maintained by the W3C.\n' +
+          '```\n' +
+          '## breakpoint\n' +
+          '`@number@` => `@12@`\n' +
+          '@12@',
         expanded: false,
       };
     },
