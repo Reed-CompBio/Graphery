c54e05abf687cc3dd95d08f84e29f86fbb76f1e5
Poppy <13360148+poppy-poppy@users.noreply.github.com>
2020-09-03 22:41:40 -0700

    added websocket handler

--- graphery/src/services/api_entry.ts
+++ graphery/src/services/api_entry.ts
@@ -2,3 +2,9 @@ const PRO_BASE_URL = 'https://api-graphery.reedcompbio.org';
 const DEV_BASE_URL = 'http://localhost:8082';
 export const BASE_URL =
   process.env.NODE_ENV === 'production' ? PRO_BASE_URL : DEV_BASE_URL;
+
+const PRO_BASE_SOCKET = 'wss://api-graphery.reedcompbio.org';
+const DEV_BASE_SOCKET = 'ws://localhost:8082';
+export const BASE_SOCKET =
+  (process.env.NODE_ENV === 'production' ? PRO_BASE_SOCKET : DEV_BASE_SOCKET) +
+  '/ws';
---
+++ graphery/src/services/websocket_handler.ts
@@ -0,0 +1,50 @@
+import { BASE_SOCKET } from '@/services/api_entry';
+
+let websocket: WebSocket | null;
+
+function createNewWebsocket(): WebSocket {
+  const socket = new WebSocket(BASE_SOCKET);
+  socket.onopen = function(event) {
+    console.debug(`new websocket opened: ${event}`);
+  };
+  socket.onclose = function(event) {
+    console.debug(`websocket closed: ${event}`);
+  };
+
+  return socket;
+}
+
+export function establishWebsocketConnection(
+  onMessage: ((this: WebSocket, ev: Event) => any) | null,
+  onOpen: ((this: WebSocket, ev: Event) => any) | null
+): WebSocket {
+  if (!websocket || websocket.readyState === WebSocket.CLOSED) {
+    websocket = createNewWebsocket();
+    websocket.onopen = onOpen;
+  }
+  websocket.onmessage = onMessage;
+  return websocket;
+}
+
+export function websocketSend(
+  message: string,
+  onMessage: ((this: WebSocket, ev: Event) => any) | null,
+  before: Function | undefined = undefined,
+  after: Function | undefined = undefined
+) {
+  if (before) {
+    before();
+  }
+
+  const socket = establishWebsocketConnection(onMessage, function(event) {
+    this.send(message);
+  });
+
+  if (socket.readyState === WebSocket.OPEN) {
+    socket.send(message);
+  }
+
+  if (after) {
+    after();
+  }
+}
