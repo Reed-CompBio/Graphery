cb75d61e94b35500084f0120693471673adbc796
Larry Zeng <13360148+FlickerSoul@users.noreply.github.com>
2020-12-23 16:19:58 -0800

    minor change

--- graphery/src/services/params.ts
+++ graphery/src/services/params.ts
@@ -21,4 +21,4 @@ def main() -> None:
 # https://github.com/FlickerSoul/Graphery/tree/master/backend/bundle#readme
 `;
 
-export const localServerTargetVersion = '0.1.2';
+export const localServerTargetVersion = '0.1.3';
