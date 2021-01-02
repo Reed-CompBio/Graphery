ae48816c78ec9ac10079b5f52721f4d315c11dbd
Larry Zeng <13360148+FlickerSoul@users.noreply.github.com>
2021-01-02 14:55:32 -0800

    make general factories

--- backend/server/tests/wrapper_test/factories.py
+++ backend/server/tests/wrapper_test/factories.py
@@ -1,19 +1,54 @@
-from typing import Tuple, Callable, Sequence
+import random
+from typing import Tuple, Callable, Sequence, Type, TypeVar, Mapping
 
-from backend.intel_wrappers.intel_wrapper import CategoryWrapper
-from backend.model.TutorialRelatedModel import Category
+from backend.intel_wrappers.intel_wrapper import CategoryWrapper, TutorialAnchorWrapper, UserWrapper
+from backend.intel_wrappers.wrapper_bases import AbstractWrapper
+from backend.model.UserModel import ROLES
+
+_T = TypeVar('_T', bound=AbstractWrapper)
+FactoryType = Tuple[Callable[[], Sequence[_T]], Callable[[Sequence[_T]], None]]
 
 
-def category_wrapper_factory(template: str, num: int) -> Tuple[Callable, Callable]:
-    def _maker() -> Sequence[CategoryWrapper]:
+def _general_factory(wrapper_type: Type[_T], params: Sequence[Mapping]) -> FactoryType:
+    def _maker() -> Sequence[_T]:
         return [
-            CategoryWrapper().load_model(
-                Category.objects.create(category=template.format(i))
-            ) for i in range(num)
+            wrapper_type().load_model(
+                wrapper_type.model_class.objects.create(**param)
+            ) for param in params
         ]
 
-    def _destructor(category_wrappers: Sequence[CategoryWrapper]) -> None:
-        for wrapper in category_wrappers:
+    def _destructor(wrappers: Sequence[_T]) -> None:
+        for wrapper in wrappers:
             wrapper.delete_model()
 
     return _maker, _destructor
+
+
+def user_wrapper_factory(username_template: str, email_template: str, num: int) -> FactoryType:
+    return _general_factory(
+        UserWrapper, [
+            {
+                'username': username_template.format(i),
+                'email': email_template.format(i),
+                'password': 'password',
+                'role': ROLES.VISITOR
+            } for i in range(num)
+        ]
+    )
+
+
+def category_wrapper_factory(template: str, num: int) -> FactoryType:
+    return _general_factory(CategoryWrapper, [
+        {'category': template.format(i)} for i in range(num)
+    ])
+
+
+def tutorial_anchor_factory(url_template: str, name_template: str, num: int, rank_base_num: int = 500) -> FactoryType:
+    return _general_factory(TutorialAnchorWrapper, [
+        {
+            'url': url_template.format(i),
+            'name': name_template.format(i),
+            'level': rank_base_num + i,
+            'section': random.randint(0, 9)
+        } for i in range(num)
+    ])
