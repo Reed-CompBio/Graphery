2bc76a1c98b578aa1f4f53779767c64d61c8eb3e
Larry Zeng <13360148+FlickerSoul@users.noreply.github.com>
2021-01-01 22:30:50 -0800

    fix "fix typo"

--- backend/server/backend/intel_wrappers/validators.py
+++ backend/server/backend/intel_wrappers/validators.py
@@ -124,5 +124,5 @@ def level_validator(level: int):
 
 
 def section_validator(section: int):
-    if not isinstance(section, int) or section > 9 or section <= 0:
+    if not isinstance(section, int) or section > 9 or section < 0:
         raise ValidationError('`section` must be a positive number that\' smaller than 10')
