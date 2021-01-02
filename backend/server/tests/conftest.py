72abd576bfe554268d4eeab536025b05b417f57f
Larry Zeng <13360148+FlickerSoul@users.noreply.github.com>
2021-01-02 00:26:27 -0800

    fix destruction

--- backend/server/tests/conftest.py
+++ backend/server/tests/conftest.py
@@ -1,4 +1,4 @@
-from typing import Callable, Sequence, Any, Optional
+from typing import Callable, Sequence, Any, Optional, List
 
 import pytest
 from django.db.models import Model
@@ -15,18 +15,22 @@ def get_fixture(request):
 
 @pytest.fixture()
 def model_factory(django_db_setup, django_db_blocker):
-    _cache: Any = None
-    _destructor: Optional[Callable] = None
+    _cache: List[Any] = []
+    _destructors: Optional[List[Callable]] = []
 
-    def _model_factory(model_maker: Callable, model_destructor: Callable = None) -> Sequence[Model]:
-        nonlocal _cache, _destructor
+    def _model_factory(model_maker: Callable, model_destructor: Callable = lambda *args: None) -> Sequence[Model]:
+        nonlocal _cache, _destructors
+        assert model_maker is not None and model_destructor is not None
+
         with django_db_blocker.unblock():
-            _cache = models = model_maker()
-            _destructor = model_destructor
+            models = model_maker()
+
+        _cache.append(models)
+        _destructors.append(model_destructor)
         return models
 
     yield _model_factory
 
-    if _destructor is not None:
+    for item, handler in zip(_cache, _destructors):
         with django_db_blocker.unblock():
-            _destructor(_cache)
+            handler(item)
