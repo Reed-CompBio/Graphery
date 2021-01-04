e991cc8e96f001a6b45725f0775e92d85643810b
Larry Zeng <13360148+FlickerSoul@users.noreply.github.com>
2021-01-04 15:20:17 -0800

    added invitation model

--- backend/server/backend/graphql/types.py
+++ backend/server/backend/graphql/types.py
@@ -13,7 +13,6 @@ from ..model.UserModel import ROLES
 from ..model.filters import show_published_only
 from ..model.mixins import field_adder, time_date_mixin_field, published_mixin_field, uuid_mixin_field
 from ..model.translation_collection import add_trans_type
-from ..model.MetaModel import InvitationCode
 from ..models import User
 from ..models import Category, Tutorial, Graph, Code, ExecResultJson
 from ..models import ENUS, ZHCN, ENUSGraphContent, ZHCNGraphContent
@@ -22,8 +21,6 @@ from graphene_django.types import DjangoObjectType
 import graphene
 
 from copy import copy
-
-InvitationCode.refresh_all_code()
 
 
 class PublishedFilterBase(DjangoObjectType):
