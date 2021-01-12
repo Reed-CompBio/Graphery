bc1ce3a062f6f51bdc4e9bdfd1db4cd3878e8e3b
Larry Zeng <13360148+FlickerSoul@users.noreply.github.com>
2021-01-11 22:23:27 -0800

    fix edge repr

--- backend/bundle/GraphObjects/Edge.py
+++ backend/bundle/GraphObjects/Edge.py
@@ -108,7 +108,7 @@ class Edge(Comparable, HasProperty, Stylable):
         return 2
 
     def __str__(self):
-        return 'Edge({}, {}}'.format(*self.node_pair)
+        return 'Edge({}, {})'.format(*self.node_pair)
 
     def __repr__(self):
         return self.__str__()
