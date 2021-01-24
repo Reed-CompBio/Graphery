9c6003093a3b8ca7e9d783a9ad27ef22bb7bf3fe
Larry Zeng <13360148+FlickerSoul@users.noreply.github.com>
2021-01-24 13:06:15 -0800

    bump up version

--- backend/setup.py
+++ backend/setup.py
@@ -11,7 +11,7 @@ long_description = 'This is a the backend of Graphery. It includes a Django serv
 
 setuptools.setup(
     name="Graphery Servers",
-    version="0.25.2",
+    version="0.25.3",
     packages=setuptools.find_packages(exclude=['tests*']),
     install_requires=read_file('requirements.txt'),
     author="Heyuan Zeng",
