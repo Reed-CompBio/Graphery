ed93adbde5615cb0c5631eee67d8cf0dae41d44a
FlickerSoul <13360148+FlickerSoul@users.noreply.github.com>
2021-04-18 10:38:54 +0800

    Update ProductGuideMixin.vue

--- graphery/src/components/mixins/ProductGuideMixin.vue
+++ graphery/src/components/mixins/ProductGuideMixin.vue
@@ -4,34 +4,74 @@
   export default {
     data() {
       return {
-        showProductGuide_: false,
+        introInstance: null,
+        shouldLoad_: true,
       };
     },
     computed: {
-      showProductGuide() {
-        return this.showProductGuide_;
-      },
-      availableSteps() {
-        return [];
+      shouldLoad() {
+        return this.shouldLoad_;
       },
     },
-    mounted() {
-      if (this.showProductGuide) {
-        import('intro.js')
-          .then((module) => {
-            console.debug('intro', module.default);
-            const introInstance = module.default();
-            introInstance.addSteps(this.availableSteps);
-            introInstance.start();
-          })
-          .catch((err) => {
-            errorDialog({ message: `Cannot load intro module. Error: ${err}` });
-          });
-      }
+    methods: {
+      addIntroSteps(steps) {
+        this.introInstance?.addSteps(steps);
+      },
+      setIntroSteps(steps) {
+        this.introInstance?.setOption('steps', steps);
+      },
+      setIntroOptions(optionKey, optionValue) {
+        this.introInstance?.setOption(optionKey, optionValue);
+      },
+      startIntro() {
+        this.introInstance?.start();
+      },
+      onCompleteCallback() {
+        return undefined;
+      },
+      onBeforeExitCallback() {
+        return true;
+      },
+      onExitCallback() {
+        return undefined;
+      },
+      async loadIntroModule() {
+        if (!this.shouldLoad) {
+          return this.introInstance;
+        }
+
+        if (this.introInstance === null) {
+          await import('intro.js')
+            .then((module) => {
+              console.debug('intro', module.default);
+              this.introInstance = module
+                .default()
+                .setOption('tooltipClass', 'customTooltip')
+                .oncomplete(this.onCompleteCallback)
+                .onbeforeexit(this.onBeforeExitCallback)
+                .onexit(this.onExitCallback);
+            })
+            .catch((err) => {
+              errorDialog({
+                message: `Cannot load intro module. Error: ${err}`,
+              });
+            });
+        }
+
+        return this.introInstance;
+      },
     },
   };
 </script>
 
-<style lang="sass" scoped>
+<style lang="sass">
   @import "~intro.js/minified/introjs.min.css"
+  @import "~intro.js/themes/introjs-modern.css"
+
+  .customTooltip
+    max-width: 500px
+    .introjs-tooltip-title
+      font-size: 1.2rem
+    .introjs-tooltiptext
+      font-size: 1rem
 </style>
