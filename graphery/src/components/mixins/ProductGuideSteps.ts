65e623f988ece8dcf2df0cb92607c2c801329d20
FlickerSoul <13360148+FlickerSoul@users.noreply.github.com>
2021-04-18 10:39:01 +0800

    Update ProductGuideSteps.ts

--- graphery/src/components/mixins/ProductGuideSteps.ts
+++ graphery/src/components/mixins/ProductGuideSteps.ts
@@ -1,5 +1,68 @@
-export const tutorialUIProductGuide = [];
+import i18n from '@/i18n';
 
-export const graphUIProductGuide = [];
+export const startIntro = [
+  {
+    title: i18n.tc('product guide.ui.intro title') + ' 👋 ',
+    intro: i18n.tc('product guide.ui.intro'),
+  },
+];
 
-export const controlUnitProductGuide = [];
+export const tutorialUIProductGuide = [
+  {
+    element: '#articleWrapper',
+    intro: i18n.tc('product guide.ui.tutorial section'),
+  },
+];
+
+export const graphUIProductGuide = [
+  {
+    element: '#cy-wrapper',
+    intro: i18n.tc('product guide.ui.graph section'),
+  },
+  {
+    element: '#cy-wrapper',
+    intro: i18n.tc('product guide.graph window.drag node and view port'),
+  },
+  {
+    element: '#cy-wrapper',
+    intro: i18n.tc('product guide.graph window.lasso tool'),
+  },
+  {
+    element: '#graph-selector',
+    intro: i18n.tc('product guide.graph window.choose graphs'),
+  },
+];
+
+export const editorProductGuide = [
+  {
+    element: '#editor-panel',
+    intro: i18n.tc('product guide.ui.editor section'),
+  },
+  {
+    element: '#variable-list-wrapper',
+    intro: i18n.tc('product guide.ui.variable list section'),
+  },
+  {
+    element: '#editor-control-unit',
+    intro: i18n.tc('product guide.ui.control unit'),
+  },
+];
+
+export const controlUnitProductGuide = [
+  {
+    element: '#control-unit-forward-button',
+    intro: i18n.tc('product guide.control unit.forward button'),
+  },
+  {
+    element: '#control-unit-step-control-button-group',
+    intro: i18n.tc('product guide.control unit.step control group'),
+  },
+  {
+    element: '#control-unit-stepper-slider',
+    intro: i18n.tc('product guide.control unit.slider'),
+  },
+  {
+    element: '#control-unit-editor-lock',
+    intro: i18n.tc('product guide.control unit.edit lock'),
+  },
+];
